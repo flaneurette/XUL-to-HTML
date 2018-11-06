e3b92b98edc163aaa04d43b2246f1b79a650fc69
flaneurette <info@flaneurette.nl>
2018-11-06 14:35:02 +0100

    yeah, yeah

--- lexer.js
+++ lexer.js
@@ -423,5 +423,13 @@
 		document.getElementById('data').innerHTML = data;
 	}
 	
-	
+	function tabs(tab) {
+    var i;
+		var x = document.getElementsByClassName("tabs");
+		for (i = 0; i < x.length; i++) {
+		   x[i].style.display = "none";  
+		}
+    document.getElementById(tab).style.display = "block";  
+	}
+
 	
